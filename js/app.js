6872f9fc924e99c8ab9075d44206e4241b1a9cd9
gabYeah <gabriel.vergnaud@hetic.net>
2014-12-10 14:05:38 +0100

    addContribution

--- js/app.js
+++ js/app.js
@@ -3,14 +3,33 @@ var app = {
 
 		UI.init();
 
-		model.getData(function(data){
-			console.log(data);
-			UI.createD3Graph(data.mots);
+		model.initFirebase();
+
+		model.getData(function(words){
+			console.log(words);
+			UI.createD3Graph(words);
 		});
 
-		model.getNodes(function(nodes){
+		/*model.getNodes(function(nodes){
 			console.log(nodes);
-		});
+		});*/
+
+		document.querySelector('#addContribution').addEventListener('keypress', app.addContribution);
+	},
+
+	addContribution: function(e){
+		if(e.keyCode == 13){
+			if(this.value){
+				var newWord = {
+					name: this.value
+				};
+
+				model.addContribution(newWord, model.words.nodes[0]);
+				
+				this.value = '';
+			}
+
+		}
 	}
 };
 
